283a9325eba2a5c12093e46c6a95783f9a92a9c3
Zach Gates <thezachgates@gmail.com>
2016-02-12 19:56:21 -0600

    6: Added missing directories

---
+++ cdn/js/init.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+	$("div#tagline").fadeIn(1500);
+
+	$("div#main-photo-cont img#center").animate({
+		top: "0px",
+		opacity: "1",
+	}, 1000);
+
+	$("div#main-photo-cont img#center").siblings().animate({
+		left: "12px",
+		opacity: "1",
+	}, 1000, function () {
+		$("div.header").slideDown(500);
+	});
+
+});
