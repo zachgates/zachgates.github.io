447b25252714031ce1c0d0130d604dce8eed9443
Zach Gates <thezachgates@gmail.com>
2016-02-12 19:27:47 -0600

    3: Improved transitions

--- js/init.js
+++ js/init.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
 
-	$("div#main-photo-cont img#center, div#tagline").fadeIn(1000, function () {
-		$("div#main-photo-cont img#center").siblings().fadeIn(1000, function () {
-			$("div.header").slideDown(500);
-		});
+	$("div#tagline").fadeIn(1500);
+
+	$("div#main-photo-cont img#center").animate({
+		top: "0px",
+		opacity: "1",
+	}, 1000);
+
+	$("div#main-photo-cont img#center").siblings().animate({
+		left: "0px",
+		opacity: "1",
+	}, 1000, function () {
+		$("div.header").slideDown(500);
 	});
 
 });
