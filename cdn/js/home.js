094abcff56bb421aaaa4d2dc6a43a6695fc6bf70
Zach Gates <thezachgates@gmail.com>
2016-02-13 02:54:57 -0600

    18: Changed landing transition

--- cdn/js/home.js
+++ cdn/js/home.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-
-	$("div#tagline").fadeIn(1500);
-
-	$("div#main-photo-cont img#center").animate({
-		top: "0px",
-		opacity: "1",
-	}, 1000);
-
-	$("div#main-photo-cont img#center").siblings().animate({
-		left: "12px",
-		opacity: "1",
-	}, 1000, function () {
-		$("div.header").slideDown(500);
+	
+	$("div.header").slideDown(500, function () {
+		$("div#tagline").fadeIn(1000);
+		
+		$("div#main-photo-cont img#center").animate({
+			top: "0px",
+			opacity: "1",
+		}, 1000);
+		
+		$("div#main-photo-cont img#center").siblings().animate({
+			left: "12px",
+			opacity: "1",
+		}, 1000);
 	});
 
 });
