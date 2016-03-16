fee052505cdcdb0759e9fddca92410aae328022c
Zach Gates <thezachgates@gmail.com>
2016-03-16 14:35:17 -0500

    Added fade

--- cdn/js/home.js
+++ cdn/js/home.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-	
-	$("div.header").slideDown(500);
+
+	$("div#main-photo-cont").hide();
+	$("div.header").slideDown(500, function () {
+		$("div#main-photo-cont").fadeIn(500);
+	});
 
 });
