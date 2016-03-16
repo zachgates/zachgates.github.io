59b43e0090ba18ed7502ed4ae944eaf1f6f4006a
Zach Gates <thezachgates@gmail.com>
2016-03-16 14:31:23 -0500

    Removed excess JavaScript

--- cdn/js/home.js
+++ cdn/js/home.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
 	
-	$("div.header").slideDown(500, function () {
-		$("div#tagline").fadeIn(1000);
-		
-		$("div#main-photo-cont img#center").animate({
-			top: "0px",
-			opacity: "1",
-		}, 1000);
-		
-		$("div#main-photo-cont img#center").siblings().animate({
-			top: "0px",
-			left: "12px",
-			opacity: "1",
-		}, 1000);
-	});
+	$("div.header").slideDown(500);
 
 });
