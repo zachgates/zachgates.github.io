faf0bcd27199409d2b7bf221289b902df2375157
Zach Gates <thezachgates@gmail.com>
2016-03-19 18:12:31 -0500

    Rotating backgrounds

--- cdn/js/home.js
+++ cdn/js/home.js
@@ -1,13 +1,52 @@
+var background2colors = {
+	0: [
+		"#ffffff",
+		"",
+		"#98021f",
+		"#ffffff",
+		"#000000",
+	],
+};
+var available = Object.keys(background2colors).map(parseFloat);
+
+var nextBackground = function (bg) {
+	// Check background exists
+	if (!available.included(bg)) {
+		bg = 0;
+	}
+	
+	// Handle background
+	$("div#nextbg").css("background-image", "/cdn/img/background" + bg ".jpg");
+	$("div#currbg").fadeOut(250);
+	
+	// Handle colors
+	colors = background2colors[bg];
+	$("div#intro-cont").css("background-color", colors[0]);
+	$("div#intro-cont").css("color", colors[1]);
+	$("div#tagline").css("background-color", colors[2]);
+	$("div#tagline").css("color", colors[3]);
+	$("div#intro-cont").css("border-color", colors[4]);
+	
+	// Return next background index
+	return bg + 1;
+}
+
 $(document).ready(function () {
 
-	$("div#main-photo-cont").hide();
+	var cbg = 0;
 
+	$("div#main-photo-cont").hide();
 	$("body").fadeIn(750, function () {
 		$("div.header").slideDown(500, function () {
 			$("div#intro-cont").fadeIn(500, function () {
 				$("div#full-cont").fadeIn(500);
 			});
 		});
+	});
+	
+	setInterval(5000, function () {
+		console.log(1);
+		var cbg = nextBackground(cbg);
 	});
 
 });
