63eebeed5830ae1a7cfd7de269b2f8db2b357602
Zach Gates <thezachgates@gmail.com>
2016-03-25 19:27:12 -0500

    Added charts and new background

--- cdn/js/init.js
+++ cdn/js/init.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 	beginLanding();
 	redirectAnchors();
 	setupScene();
+	setupSkills();
 	setTimeout(redirect, 250);
 });
 
@@ -58,14 +59,16 @@ var handleCover = function (w, h) {
 var handleCenter = function () {
 	function applyPad (needw, needh) {
 		var pad = calcPadPx(this);
-		$(this).css({
-			left: (needw || true) ? pad[0] : "",
-			top : (needh || true) ? pad[1] : "",
-		});
+		if (needw || false) {
+			$(this).css("left", pad[0]);
+		}
+		if (needh || false) {
+			$(this).css("top", pad[1]);
+		}
 	}
-	$("div.center").each(applyPad);                  // Center width and height.
-	$("div.center-w").each(applyPad, [false, true]); // Center width-only.
-	$("div.center-h").each(applyPad, [true, false]); // Center height-only.
+	$("div.center").each(applyPad, [true, true]);    // Center width and height.
+	$("div.center-w").each(applyPad, [true, false]); // Center width-only.
+	$("div.center-h").each(applyPad, [false, true]); // Center height-only.
 }
 
 /* Setup */
@@ -116,4 +119,67 @@ var scrollTo = function (query) {
 	var pos = $("#" + query).offset().top;
 	$("html, body").animate({scrollTop: pos});
 	window.location.hash = "#/" +(query == "intro" ? "" : query);
+}
+
+/* Setup chart */
+
+var setupSkills = function () {
+	var skillsCont = $("canvas#skills-chart").get(0).getContext("2d"),
+		p2l = {"100":"Master","80":"Expert","60":"Proficient","40":"Familiar","20":"Beginner"}
+	var skillsChart = new Chart(skillsCont).Bar({
+			labels: [
+				"Python",
+				"Django",
+				"PHP",
+				"HTML",
+				"CSS",
+				"JavaScript",
+				"jQuery",
+				"Objective-C",
+				"Git",
+				"Windows",
+				"OS X",
+				"Linux",
+				"Photoshop",
+				"Vim",
+				"Wordpress",
+				"Chrome",
+			],
+			datasets: [
+				{
+					fillColor: "#7b97ca",
+					strokeColor: "#7b97ca",
+					data: [
+						100,
+						80,
+						60,
+						100,
+						80,
+						80,
+						80,
+						60,
+						100,
+						80,
+						100,
+						100,
+						80,
+						80,
+						60,
+						100,
+					]
+				}
+			],
+		},
+		{
+			responsive : true,
+			scaleOverride: true,
+			scaleSteps: 5,
+			scaleStepWidth: 20,
+			scaleStartValue: 0,
+			scaleShowHorizontalLines: false,
+			scaleShowVerticalLines: false,
+			barValueSpacing: $(this).width() * 0.0075,
+			tooltipTemplate: '<%= {"100":"Master","80":"Expert","60":"Proficient","40":"Familiar","20":"Beginner"}[value] %>',
+			scaleFontSize: 14,
+		});
 }
